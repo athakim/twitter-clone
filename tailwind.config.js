e04b472b3ddf6dd7467d8d8a705d6da86574cde6
athakim <athakim95@gmail.com>
2022-05-09 08:34:45 +0200

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+  theme: {
+   
+    extend: {
+      colors:{
+        'twitter-color': '#1D9BF0',
+        'gris-twitter':'#F7F9F9',
+      },
+    },
+  },
+  plugins: [],
+}
